327010a9097533bbef6101eeee9e464c6436ab0f
Joni Jansson <joni.jansson@saunalahti.fi>
2022-02-07 15:54:29 +0200

    movies.js work in progress

--- ws3/movies.js
+++ ws3/movies.js
@@ -4,9 +4,17 @@ const fetch = require('node-fetch');
 // Declare API url in variabl
 const apiURL = "http://www.omdbapi.com/?s=star+wars&apikey=cbbc6750"
 
+// Object to hold json data
+let json = {};
 // GET -request as default
 fetch(apiURL)
     // Parse response
   .then(response => response.json())
    // Display data in console (object)
-  .then(data => console.log(data));
+  .then(data => saveDataToVariable(data));
+
+function saveDataToVariable(data) {
+    json = data;
+    console.log(json);
+}
+
